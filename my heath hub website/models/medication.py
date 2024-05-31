d0b1849d3548f360d8af6984c262aa563d904a9d
Danmuumbi <muuomuumbi@gmail.com>
2024-05-31 12:36:41 +0300

    done

--- my heath hub website/models/medication.py
+++
@@ -1,8 +0,0 @@
-from models.base_model import BaseModel
-
-class Medication(BaseModel):
-    def __init__(self, *args, **kwargs):
-        self.name = ""
-        self.dosage = ""
-        self.patient_id = ""
-        super().__init__(*args, **kwargs)
--- my heath hub website/models/patient.py
+++
@@ -1,9 +0,0 @@
-from models.base_model import BaseModel
-
-class Patient(BaseModel):
-    def __init__(self, *args, **kwargs):
-        self.name = ""
-        self.age = 0
-        self.medical_history = ""
-        self.contact_info = ""
-        super().__init__(*args, **kwargs)
